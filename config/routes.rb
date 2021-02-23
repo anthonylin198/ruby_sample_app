f4fd4998da0a7cfc91dbbd6a6cb0462a6011633b
anthonylin198 <anthony.lin.198@gmail.com>
2021-02-23 15:20:37 -0800

    converting paths to home_path etc...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get '/bye', to: 'application#bye' 
+
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
